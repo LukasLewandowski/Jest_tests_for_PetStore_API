058f5e569a2faac542d8929617b4f8b86cf76b4c
LukasLewandowski <llewandowski1990@gmail.com>
2021-04-28 23:15:51 +0200

    use spread operator for payload object

--- step_definitions/petStore.steps.js
+++ step_definitions/petStore.steps.js
@@ -6,36 +6,44 @@ const feature = loadFeature('features/petStore.feature');
 
 const baseUrl = 'https://petstore.swagger.io/v2';
 const petStoreEndpoint = '/pet';
+
 const request = supertest(baseUrl);
-const sampleDogImage = 'https://pixabay.com/images/id-4372036/';
-const sampleCatImage = 'https://pixabay.com/images/id-2934720/';
 const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
 };
 
+const sampleDogImage = 'https://pixabay.com/images/id-4372036/';
+const sampleCatImage = 'https://pixabay.com/images/id-2934720/';
+
 defineFeature(feature, (test) => {
+    const defaultPayload = {
+        // id: petId,
+        category: {
+            id: 0,
+            name: 'dogs',
+        },
+        tags: [
+            {
+                id: 0,
+                name: 'dog',
+            },
+        ],
+        status: 'available',
+    };
+
     test('Add a new pet to the store', ({ given, when, then }) => {
         const petId = Date.now();
-
         console.log(petId);
 
         const payload = {
             id: petId,
-            category: {
-                id: 0,
-                name: 'dogs',
-            },
             name: faker.name.firstName(),
             photoUrls: [sampleDogImage],
-            tags: [
-                {
-                    id: 0,
-                    name: 'dog',
-                },
-            ],
-            status: 'available',
+            ...defaultPayload,
         };
+
+        console.log('pacz: ' + JSON.stringify(payload));
 
         when('I add a new pet', async () => {
             const response = await request
@@ -51,9 +59,7 @@ defineFeature(feature, (test) => {
                 .get(`${petStoreEndpoint}/${petId}`)
                 .set(headers);
             expect(response.status).toEqual(200);
-            expect(response.body).not.toBe(null);
-            expect(response.body.id).toEqual(petId);
-            expect(response.body.name).toEqual(payload.name);
+            expect(response.body).toEqual(payload);
         });
     });
 
@@ -75,20 +81,12 @@ defineFeature(feature, (test) => {
 
         const payload = {
             id: petId,
-            category: {
-                id: 0,
-                name: 'cats',
-            },
             /* missing name */
-            photoUrls: [sampleCatImage],
-            tags: [
-                {
-                    id: 0,
-                    name: 'cat',
-                },
-            ],
-            status: 'available',
+            photoUrls: [sampleDogImage],
+            ...defaultPayload,
         };
+
+        console.log('pacz2: ' + JSON.stringify(payload));
 
         when('I add new pet without name', async () => {
             const response = await request
@@ -113,20 +111,12 @@ defineFeature(feature, (test) => {
 
         const payload = {
             id: petId,
-            category: {
-                id: 0,
-                name: 'cats',
-            },
             name: faker.name.firstName(),
             /* missing photo */
-            tags: [
-                {
-                    id: 0,
-                    name: 'cat',
-                },
-            ],
-            status: 'available',
+            ...defaultPayload,
         };
+
+        console.log('pacz3: ' + JSON.stringify(payload));
 
         when('I add new pet without photo', async () => {
             const response = await request
